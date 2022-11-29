c1e5750372db78a30945f1b63898fa66abbde190
chelseaesanson8 <chelseaesanson@outlook.com>
2022-11-29 11:04:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio-site
+ a site to display...all about me and my projects
---
+++ index.js
@@ -0,0 +1,16 @@
+function typingEffect(element, speed) {
+    let text = element.innerHTML;
+    element.innerHTML = "";
+    var i = 0;
+    var timer = setInterval(function () {
+        if (i < text.length) {
+            element.append(text.charAt(i))
+            i++;
+        } else {
+            clearInterval(timer);
+        }
+    }, speed)
+}
+
+const h1 = document.querySelector('h1');
+typingEffect(h1, 150);
